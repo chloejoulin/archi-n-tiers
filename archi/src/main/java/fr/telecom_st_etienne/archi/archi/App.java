208976ce42279b44874f4bb71fcc60f01187f5ee
Chloe Joulin <chloe.joulin@outlook.com>
2019-10-22 17:28:13 +0200

    creating project

---
+++ archi/src/main/java/fr/telecom_st_etienne/archi/archi/App.java
@@ -0,0 +1,13 @@
+package fr.telecom_st_etienne.archi.archi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
